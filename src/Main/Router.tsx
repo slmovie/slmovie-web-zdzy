71277c150c73b3cd649acc0eea72bb68f867f2b8
秋月夜 <baojun.cz@gmail.com>
2019-01-15 14:48:09 +0800

    remove eslint,add typescript

--- src/Main/Router.tsx
+++ src/Main/Router.tsx
@@ -2,7 +2,7 @@
  * @Author: BaojunCZ
  * @Date: 2019-01-03 19:30:20
  * @LastEditors: your name
- * @LastEditTime: 2019-01-15 13:53:12
+ * @LastEditTime: 2019-01-15 14:30:06
  */
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -12,7 +12,7 @@ import HomePage from "../containers/HomePage";
 const history = createBrowserHistory();
 
 const Router = () => (
-  <BrowserRouter history={history}>
+  <BrowserRouter>
     <Switch>
       <Route path="/" exact component={HomePage} history={history} />
     </Switch>
---
+++ src/constant/Url.ts
@@ -0,0 +1,23 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: BaojunCZ
+ * @Date: 2019-01-04 17:18:09
+ * @LastEditTime: 2019-01-15 14:07:42
+ * @LastEditors: your name
+ */
+// exports.WebRoot = "http://www.slys.in";
+// exports.SearchAll = "/search/all";
+// exports.Detail = "/detail";
+// exports.IndexHotMovies = "/index/hotMovies";
+// exports.IndexNewMovies = "/index/newMovies?index=";
+// exports.IndexNewTVs = "/index/newTVs?index=";
+const Urls = {
+  WebRoot: "http://www.slys.in",
+  SearchAll: "/search/all",
+  Detail: "/detail",
+  IndexHotMovies: "/index/hotMovies",
+  IndexNewMovies: "/index/newMovies?index=",
+  IndexNewTVs: "/index/newTVs?index=",
+};
+
+export default Urls;
