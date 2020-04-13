225a36b718a9343af03d1dfdf512d733164ae363
Bao Jun <baojun.cz@gmail.com>
2020-04-13 20:58:43 +0800

    chore: update port

--- src/constant/Url.ts
+++ src/constant/Url.ts
@@ -6,7 +6,7 @@
  * @LastEditors: your name
  */
 const Urls = {
-  WebRoot: "http://152.136.103.63:3000",
+  WebRoot: "http://152.136.103.63:3010",
   SearchAll: "/search?name=",
   Detail: "/detail?code=",
   IndexHotMovies: "/index/hot",
