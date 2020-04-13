b7c91634f9437094bf24927233a502a1b21337cb
Bao Jun <baojun.cz@gmail.com>
2020-04-13 20:49:35 +0800

    chore: build

---
+++ build/precache-manifest.dcec5f8b5b663196d84518edde8ef297.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8d482cecec578225adb4a2d46ff9738",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18a97910c095e5029dde",
+    "url": "/static/css/2.78a89035.chunk.css"
+  },
+  {
+    "revision": "dcb7799ff46db4754a6d",
+    "url": "/static/css/main.c5a52361.chunk.css"
+  },
+  {
+    "revision": "18a97910c095e5029dde",
+    "url": "/static/js/2.f994603c.chunk.js"
+  },
+  {
+    "revision": "f59c5aad956749e5384264a0c1b84455",
+    "url": "/static/js/2.f994603c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcb7799ff46db4754a6d",
+    "url": "/static/js/main.0dd6fb03.chunk.js"
+  },
+  {
+    "revision": "e8ee5145a065f0866ee2",
+    "url": "/static/js/runtime-main.d4aaad3f.js"
+  }
+]);
