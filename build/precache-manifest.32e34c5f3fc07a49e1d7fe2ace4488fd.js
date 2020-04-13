bed1b691aa5d70461efafb700849bd42174ee3e7
BaojunCZ <baojun.cz@gmail.com>
2020-04-13 23:23:33 +0800

    chore: build

---
+++ build/precache-manifest.32e34c5f3fc07a49e1d7fe2ace4488fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14fbf1a944fe628da06fb66ee0430a8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3587a045318a31534c54",
+    "url": "/static/css/2.a9905d1b.chunk.css"
+  },
+  {
+    "revision": "40583bcab14fed03143f",
+    "url": "/static/css/main.9121500c.chunk.css"
+  },
+  {
+    "revision": "3587a045318a31534c54",
+    "url": "/static/js/2.f994603c.chunk.js"
+  },
+  {
+    "revision": "f59c5aad956749e5384264a0c1b84455",
+    "url": "/static/js/2.f994603c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40583bcab14fed03143f",
+    "url": "/static/js/main.55b33737.chunk.js"
+  },
+  {
+    "revision": "e8ee5145a065f0866ee2",
+    "url": "/static/js/runtime-main.d4aaad3f.js"
+  }
+]);
